63eeb19ac87e0761cd5254e163826b1ae1510b99
ksteve66 <kyocherstephen@gmail.com>
2022-07-05 19:12:14 -0700

    Hot fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-*  _isalpha - checks for alphabetical letters
-*  @c: a character to be checked on
-*  Return: returns 0 or 1 depending on condition
+* _isalpha - checks for alphabetical letters
+* @c: a character to be checked on
+* Return: returns 0 or 1 depending on condition
 */
 int _isalpha(int c)
 {
