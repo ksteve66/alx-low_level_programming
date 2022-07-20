43133ec341ab83b4266554b22055c71092e7b39e
ksteve66 <kyocherstephen@gmail.com>
2022-07-20 15:35:19 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,48 +1,34 @@
 #include "main.h"
-int lengthc(char *s);
-int palindrome(char str[], int st, int end);
+
 /**
- * is_palindrome - Entry Point
- * @s: input
- * Return: 0
+ * is_prime - detects if an input number is a prime number.
+ * @n: input number.
+ * @c: iterator.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
-int is_palindrome(char *s)
+int is_prime(unsigned int n, unsigned int c)
 {
-	int len;
-
-	len = lengthc(s);
-
-	if (len == 0)
-		return (1);
-	return (palindrome(s, 0, len - 1));
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + is_prime(n, c + 1));
 }
-
 /**
- * lengthc - finds the length count
- * @s: input
- * Return: length size
+ * is_prime_number - detects if an input number is a prime number.
+ * @n: input number.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
-int lengthc(char *s)
+int is_prime_number(int n)
 {
-	if (*s != '\0')
-		return (1 + lengthc(s + 1));
-	return (0);
-}
-
-/**
- * palindrome - checks if start and end of string matches
- * @str: string
- * @st: start of string 0
- * @end: end of string from is_palindrome, from lengthc
- * Return: if str is a palindrome
- */
-int palindrome(char str[], int st, int end)
-{
-	if (st >= end)
-		return (1);
-	if (str[st] != str[end])
+	if (n == 0)
 		return (0);
-	if (st <= end || st < end + 1)
-		return (palindrome(str, st + 1, end - 1));
-	return (1);
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (is_prime(n, 2));
 }
