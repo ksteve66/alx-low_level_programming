16557945ba92af463d412be806285238be7b309c
ksteve66 <kyocherstephen@gmail.com>
2022-08-03 14:52:08 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,80 +1,44 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _atoi - Converts a string to an integer
- * @s: pointer to the first character of the string
+ * main - Prints the opcodes of itself.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: Value of integer in string
+ * Return: Always 0.
  */
-int _atoi(char *s)
+int main(int argc, char *argv[])
 {
-	unsigned int num;
-	int neg;
-
-	neg = 1;
-	num = 0;
-
-	for (; *s; s++)
-	{
-		if (*s >= '0' && *s <= '9')
-		{
-			num *= 10;
-			num += *s - '0';
-		}
-		else if (num > 0)
-		{
-			break;
-		}
-		else if (*s == '-')
-		{
-			neg = -neg;
-		}
-	}
-
-	return (num * neg);
-}
-
-/**
- * main - Prints the opcodes of its own main function
- * @argc: Number of arguments
- * @argv: array of pointers to arguments
- *
- * Return: 0 For success, otherwise 1
- */
-int main(int argc, char **argv)
-{
-	char *src =
-		"554889e54883ec20897dec488975e048c745f808084000837dec027414bfc8094000"
-		+ "e819feffffbf01000000e84ffeffff488b45e04883c008488b004889c7e839ffffff8"
-		+ "945f4837df4007914bfc8094000e8e9fdffffbf01000000e81ffeffffc745f0000000"
-		+ "00eb4e837df000740abf20000000e8b6fdffff8b45f04898488d5001488b45f84801d"
-		+ "00fb6000fbed08b45f04863c8488b45f84801c80fb6000fbec089c6bfce094000b800"
-		+ "000000e89cfdffff8345f0028b45f04863d0488b45f84801d00fb60084c074118b45f"
-		+ "089c2c1ea1f01d0d1f83b45f47c8dbf0a000000e849fdffffb800000000c9c36690";
-
-	int i, n;
+	int bytes, index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
+	bytes = atoi(argv[1]);
 
-	n = _atoi(argv[1]);
-	if (n < 0)
+	if (bytes < 0)
 	{
 		printf("Error\n");
-		exit(1);
+		exit(2);
+	}
+	for (index = 0; index < bytes; index++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (index == bytes - 1)
+			continue;
+		printf(" ");
+
+		address++;
 	}
 
-	for (i = 0; src[i] && i / 2 < n; i += 2)
-	{
-		if (i != 0)
-			printf(" ");
-		printf("%c%c", src[i], src[i + 1]);
-	}
-	printf("\n");
+	print("\n");
 
 	return (0);
 }
